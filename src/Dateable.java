a049f6cee048b75da0db3bb53e056e373fc1d614
roninst2027 <roninst2027@student.cis.edu.hk>
2025-02-12 07:11:23 +0000

    Tutorial Done

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 public class Dateable {
 
   public static void main(String[] args) {
       // Write your code below, inside the main method.
-    
+   //Creating a scanner for input
+   Scanner scanner = new Scanner(System.in);
+
+   // prompt user to enter their age
+   System.out.print("How old are you?");
+   int age = scanner.nextInt();
+
+   //Calculate the minimum age
+   int minimumAge = (7+(age/2));
+
+   //print the users minimum dating age
+   System.out.print("You should only date people aged " +minimumAge + " or above.");
+
   }
   
 }
